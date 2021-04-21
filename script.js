a3c92c4ed419f5199b42bf99070c6ca93c42855c
SBC <lochan.sharad@gmail.com>
2021-04-21 22:08:50 +0800

    Added appid in fetch request to get the requested data

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=d168d3a7571fda5f948e93fcef34e556')
+var cardtext = document.querySelector("#cardtext");
+
+var appid = "d168d3a7571fda5f948e93fcef34e556";
+var cityname = "sydney";
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+ cityname + '&exclude=minutely&units=imperial&appid=' + appid)
   .then(function (response) {
-    console.log(response);
+    
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+ console.log(data);
   });
